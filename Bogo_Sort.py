9673da5c80134caa99c15e6bf027584fc29a0965
KingTantin <ctranschel@outlook.de>
2026-02-05 18:35:45 +0100

    Update Bogo_Sort.py

--- Bogo_Sort.py
+++ Bogo_Sort.py
@@ -1,23 +1,29 @@
 import random
 import time
-#Generate list_length random numbers between 1 and listh_length+1
-list_length = 10
-unordered_list = random.sample(range(1, 2*list_length), list_length)
 
 def bogo_sort(unordered_list: list[int]) -> list[int]:
-  while True:
-      is_sorted = True
-      random.shuffle(unordered_list)
-  
-      for i in range(0, len(unordered_list)-1):
-          if unordered_list[i] > unordered_list[i+1]:
-              is_sorted = False
-              break
-  
-      if is_sorted == True:  
-          print(unordered_list)
-          break
+    while True:
+        is_sorted = True
+        random.shuffle(unordered_list)
+
+        for i in range(0, len(unordered_list)-1):
+            if unordered_list[i] > unordered_list[i+1]:
+                is_sorted = False
+                break
+
+        if is_sorted == True:  
+            print(unordered_list)
+            break
+    return unordered_list
+
+
+#Generate list_length random numbers between 1 and listh_length+1
+list_length = 9
+unordered_list = random.sample(range(1, 2*list_length), list_length)
+
 start = time.time()
-bogo_sort(unordered_list)
+ordered_list = bogo_sort(unordered_list)
 end =  time.time()
+
+print(ordered_list)
 print(end-start)
